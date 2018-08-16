c14c9456e7cc854a9fdeb02bab3a57c65372eeaa
jgburton <jgranthamburton@gmail.com>
2018-08-16 14:52:40 +0100

    single blog layout

--- single.php
+++ single.php
@@ -7,31 +7,33 @@
  * @package StanleyWP
  */
 
-get_header(); ?>
+ get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
 
-				<?php
-				while ( have_posts() ) : the_post();
+ 		<div id="primary" class="content-area-full">
+ 			<main id="main" class="site-main" role="main">
 
-					get_template_part( 'template-parts/content', get_post_format() );
+ 			<?php
+ 			while ( have_posts() ) : the_post();
+ 				get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-					the_post_navigation();
+ 				<div class="container">
+ 						<div class="row justify-content-center">
+ 							<div class="col-md-8">
+ 								<?php the_post_navigation();
+ 								// If comments are open or we have at least one comment, load up the comment template.
+ 								if ( comments_open() || get_comments_number() ) :
+ 									comments_template();
+ 								endif; ?>
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+ 							</div><!--  .col-md-8 -->
+ 						</div><!--  .row -->
+ 					</div><!--  .container -->
 
-				endwhile; // End of the loop.
-				?>
+ 			<?php endwhile; // End of the loop. ?>
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
+ 			</main><!-- #main -->
+ 		</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+ <?php
+ get_footer();
