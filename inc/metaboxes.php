a5808e00845a17fd156a8c394d4aa34a30db0b03
jgburton <jgranthamburton@gmail.com>
2018-08-22 20:29:23 +0100

    single project template

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -41,6 +41,29 @@ function cmb2_stanleywp_metaboxes() {
   ) );
 
 
+  /**
+  * Metabox for project
+  */
+  $cmb_project = new_cmb2_box( array(
+    'id'            => 'project_metabox',
+    'title'         => __( 'Images', 'stanleywp' ),
+    'object_types'  => array( 'project', ), // Post type
+    'context'       => 'normal',
+    'priority'      => 'high',
+    'show_names'    => true, // Show field names on the left
+    // 'cmb_styles' => false, // false to disable the CMB stylesheet
+    // 'closed'     => true, // Keep the metabox closed by default
+  ) );
+
+  // Images for project
+  $cmb_project->add_field( array(
+    'name'       => __( 'Images', 'stanleywp' ),
+    'desc'       => __( 'Upload Images', 'stanleywp' ),
+    'id'         => $prefix . 'images',
+    'type'       => 'file_list',
+  ) );
+
+
 
   // Add other metaboxes as needed
 
